cc0173fc78055f2f9b86a3d101f3c36a38f40e97
Lucas M. de Jong Larrarte <lucasmdjl@protonmail.com>
2025-07-02 01:15:52 +0200

    Improve tests

--- src/test/scala/ClockTest.scala
+++ src/test/scala/ClockTest.scala
@@ -20,7 +20,7 @@ package dev.lucasmdjl.scala.clock
 
 import org.scalatest.flatspec.AnyFlatSpec
 
-import java.util.concurrent.{CountDownLatch, Executors}
+import java.util.concurrent.{CyclicBarrier, Executors}
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.{Await, ExecutionContext, Future}
 
@@ -60,11 +60,11 @@ class ClockTest extends AnyFlatSpec {
     assertResult(100, "now after delay")(clock.now())
   }
 
-  "Clock.fixed" should "throw when negative time" in {
+  it should "throw when negative time" in {
     assertThrows[IllegalArgumentException](Clock.fixed(-100))
   }
 
-  "Clock.fixed" should "not throw when zero time" in {
+  it should "not throw when zero time" in {
     Clock.fixed()
   }
 
@@ -79,20 +79,36 @@ class ClockTest extends AnyFlatSpec {
     assertResult(1_200, "now after second tick")(clock.now())
   }
 
-  "Clock.incremental" should "throw when negative initial time" in {
+  it should "throw when negative initial time" in {
     assertThrows[IllegalArgumentException](Clock.incremental(-100, 100))
   }
 
-  "Clock.incremental" should "not throw when zero initial time" in {
+  it should "not throw when zero initial time" in {
     Clock.incremental(0, 10)
   }
 
-  "Clock.incremental" should "throw when zero step" in {
+  it should "throw when zero step" in {
     assertThrows[IllegalArgumentException](Clock.incremental(100, 0))
   }
 
-  "Clock.incremental" should "throw when negative step" in {
+  it should "throw when negative step" in {
     assertThrows[IllegalArgumentException](Clock.incremental(100, -10))
+  }
+
+  it should "be thread-safe" in {
+    given ExecutionContext =
+      ExecutionContext.fromExecutor(Executors.newFixedThreadPool(11))
+    val start = CyclicBarrier(10)
+    val clock = Clock.incremental()
+    val futures = for (i <- 1 to 10) yield {
+      Future {
+        start.await()
+        clock.tick()
+      }
+    }
+    val future = Future.sequence(futures)
+    Await.result(future, 1.second)
+    assertResult(10, "now")(clock.now())
   }
 
   "Clock.manual" should "return a clock with manual time" in {
@@ -106,74 +122,54 @@ class ClockTest extends AnyFlatSpec {
     assertResult(200, "now after setting time")(clock.now())
   }
 
-  "Clock.manual" should "throw when negative initial time" in {
+  it should "throw when negative initial time" in {
     assertThrows[IllegalArgumentException](Clock.manual(-100))
   }
 
-  "Clock.manual" should "not throw when zero initial time" in {
+  it should "not throw when zero initial time" in {
     Clock.manual()
   }
 
-  "Clock.manual" should "throw when advancing negative time" in {
+  it should "throw when advancing negative time" in {
     val clock = Clock.manual(10)
     assertThrows[IllegalArgumentException](clock.advance(-100))
   }
 
-  "Clock.manual" should "not throw when advancing zero time" in {
+  it should "not throw when advancing zero time" in {
     val clock = Clock.manual(10)
     clock.advance(0)
   }
 
-  "Clock.manual" should "throw when setting negative time" in {
+  it should "throw when setting negative time" in {
     val clock = Clock.manual(1_000)
     assertThrows[IllegalArgumentException](clock.setTime(-1_000))
   }
 
-  "Clock.manual" should "throw when setting time in past" in {
+  it should "throw when setting time in past" in {
     val clock = Clock.manual(1_000)
     assertThrows[IllegalArgumentException](clock.setTime(100))
   }
 
-  "Clock.manual" should "throw when setting time in past after advancing" in {
+  it should "throw when setting time in past after advancing" in {
     val clock = Clock.manual(1_000)
     clock.advance(500)
     assertThrows[IllegalArgumentException](clock.setTime(1_100))
   }
 
-  "Clock.manual" should "throw when setting time in past after setting time" in {
+  it should "throw when setting time in past after setting time" in {
     val clock = Clock.manual(100)
     clock.advance(500)
     assertThrows[IllegalArgumentException](clock.setTime(300))
   }
 
-  "Clock.incremental" should "be thread-safe" in {
+  it should "be thread-safe" in {
     given ExecutionContext =
       ExecutionContext.fromExecutor(Executors.newFixedThreadPool(11))
-    val startCdl = CountDownLatch(10)
-    val endCdl = CountDownLatch(10)
-    val clock = Clock.incremental()
-    val futures = for (i <- 1 to 10) yield {
-      Future {
-        startCdl.countDown()
-        startCdl.await()
-        clock.tick()
-        endCdl.countDown()
-      }
-    }
-    val future = Future.sequence(futures)
-    Await.result(future, 1.second)
-    assertResult(10, "now")(clock.now())
-  }
-
-  "Clock.manual" should "be thread-safe" in {
-    given ExecutionContext =
-      ExecutionContext.fromExecutor(Executors.newFixedThreadPool(11))
-    val startCdl = CountDownLatch(10)
+    val start = CyclicBarrier(10)
     val clock = Clock.manual()
     val futures = for (i <- 1 to 10) yield {
       Future {
-        startCdl.countDown()
-        startCdl.await()
+        start.await()
         clock.advance(i)
       }
     }
